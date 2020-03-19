cabd99eed79a96aad73d3730f9269a401a1be139
Quazeye <quazeye@shaw.ca>
2020-03-19 14:04:21 -0600

    Buttons

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    Scene scene;
+
+    public void LoadNextScene()
+    {
+        scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.buildIndex + 1);
+    }
+}
